fd9d8910e4f0c048b28786afb84203e24ae72ab3
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-03-09 18:44:09 -0500

    Add redux and react router begin to set up store

---
+++ src/redux/Store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import rootReducer from "./rootReducer.js";
+
+
+
+const middlewares = [logger];
+
+if (process.env.NODE_ENV === 'development') {
+     middlewares.push(logger);
+   }
+ 
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+
+
+
+
